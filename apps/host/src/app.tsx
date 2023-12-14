8a8fbc794dd98a7c17da61bdc8b84aee7ff85b3e
Gustavo Benitez <gustavo.benitez.developer@gmail.com>
2023-12-14 09:09:06 -0300

    Add screen placeholder and screen switcher

--- apps/host/src/app.tsx
+++ apps/host/src/app.tsx
@@ -4,6 +4,56 @@ const HarryPotter = React.lazy(() => import("harry_potter/app"));
 
 import { Button } from "ui-lib";
 
+interface ScreenPlaceholderProps {
+  screen: string;
+}
+
+function ScreenPlaceholder({ screen }: ScreenPlaceholderProps) {
+  return (
+    <Suspense fallback={"loading..."}>
+      {screen === "RICKY" && (
+        <>
+          <div>
+            <RickyAndMorty />
+          </div>
+        </>
+      )}
+      {screen === "HARRY" && (
+        <>
+          <div>
+            <HarryPotter />
+          </div>
+        </>
+      )}
+    </Suspense>
+  );
+}
+
+interface ScreenSwitcherProps {
+  onScreenChanged: (screen: string) => void;
+}
+
+function ScreenSwitcher({ onScreenChanged }: ScreenSwitcherProps) {
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          onScreenChanged("RICKY");
+        }}
+      >
+        <Button.Text>Rick and Morty</Button.Text>
+      </Button>
+      <Button
+        onClick={() => {
+          onScreenChanged("HARRY");
+        }}
+      >
+        <Button.Text>Harry Potter</Button.Text>
+      </Button>
+    </div>
+  );
+}
+
 export default function App() {
   const [selectedScreen, setSelectedScreen] = React.useState<string>("RICKY");
 
@@ -12,37 +62,11 @@ export default function App() {
   };
 
   return (
-    <Suspense fallback={"loading..."}>
+    <div>
+      <ScreenSwitcher onScreenChanged={handleScreenSwitch} />
       <div>
-        <Button
-          onClick={() => {
-            handleScreenSwitch("RICKY");
-          }}
-        >
-          <Button.Text>Rick and Morty</Button.Text>
-        </Button>
-        <Button
-          onClick={() => {
-            handleScreenSwitch("HARRY");
-          }}
-        >
-          <Button.Text>Harry Potter</Button.Text>
-        </Button>
+        <ScreenPlaceholder screen={selectedScreen} />
       </div>
-      {selectedScreen === "RICKY" && (
-        <>
-          <div>
-            <RickyAndMorty />
-          </div>
-        </>
-      )}
-      {selectedScreen === "HARRY" && (
-        <>
-          <div>
-            <HarryPotter />
-          </div>
-        </>
-      )}
-    </Suspense>
+    </div>
   );
 }
