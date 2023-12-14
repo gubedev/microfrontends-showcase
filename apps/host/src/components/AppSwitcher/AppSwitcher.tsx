89da624a45746fca44a8390b50f8232aa6d6a77a
Gustavo Benitez <gustavo.benitez.developer@gmail.com>
2023-12-14 19:21:18 -0300

    Sync switcher selected

--- apps/host/src/components/AppSwitcher/AppSwitcher.tsx
+++ apps/host/src/components/AppSwitcher/AppSwitcher.tsx
@@ -21,21 +21,30 @@ interface AppSwitcherProps {
  * @returns {JSX.Element} - The rendered component.
  */
 export function AppSwitcher({ onAppChange }: AppSwitcherProps): JSX.Element {
+  const currentApp = React.useRef<AppItem>(AppItem.RICK_AND_MORTY);
+
+  const handlAppChange = (app: AppItem) => {
+    currentApp.current = app;
+    onAppChange(app);
+  };
+
   return (
     <div>
       <Button
         onClick={() => {
-          onAppChange(AppItem.RICK_AND_MORTY);
+          handlAppChange(AppItem.RICK_AND_MORTY);
         }}
         data-testid="app-switcher-rick-and-porty-test-id"
+        selected={currentApp.current === AppItem.RICK_AND_MORTY}
       >
         <Button.Text>Rick and Morty</Button.Text>
       </Button>
       <Button
         onClick={() => {
-          onAppChange(AppItem.HARRY_POTTER);
+          handlAppChange(AppItem.HARRY_POTTER);
         }}
         data-testid="app-switcher-harry-potter-test-id"
+        selected={currentApp.current === AppItem.HARRY_POTTER}
       >
         <Button.Text>Harry Potter</Button.Text>
       </Button>
