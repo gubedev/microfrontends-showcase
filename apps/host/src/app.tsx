ff50b7e4318cb9a15ebb106eafa9023a1b3a5bff
Gustavo Benitez <gustavo.benitez.developer@gmail.com>
2023-12-15 00:23:58 -0300

    Add layout

--- apps/host/src/app.tsx
+++ apps/host/src/app.tsx
@@ -1,14 +1,13 @@
-import { AppPlaceholder, AppSwitcher, LangSwitcher, Layout } from "components";
+import React from "react";
+import { AppPlaceholder, Container, Navbar, Wrapper } from "components";
 import { EventNotificationFactory } from "event-notification-lib";
 import { StorageServiceFactory } from "storage-lib";
 import { MessageItem, settings } from "core";
-import React from "react";
 import { AppItem, LangItem } from "types";
 
 export default function App() {
-  // State to track the currently selected app
   const [selectedApp, setSelectedApp] = React.useState<AppItem>(
-    AppItem.RICK_AND_MORTY // Initial selected app is set to Rick and Morty
+    AppItem.RICK_AND_MORTY
   );
 
   const handleAppSwitch = (app: AppItem) => {
@@ -39,21 +38,15 @@ export default function App() {
     (getStoreLang() as LangItem) ?? (settings.defaultLang as LangItem);
 
   return (
-    <Layout>
-      <div style={{ marginTop: "128px" }}>
-        <h1>Microfrontends</h1>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {/* Render the AppSwitcher component with the handleAppSwitch function as a prop */}
-          <AppSwitcher onAppChange={handleAppSwitch} />
-          {/* Render the LangSwitcher component with the handleLangSwitch function as a prop */}
-          <LangSwitcher onLangChange={handleLangSwitch} initial={initialLang} />
-        </div>
-        {/* Container to display the selected app using the AppPlaceholder component */}
-        <div>
-          {/* Render the AppPlaceholder component with the currently selected app */}
-          <AppPlaceholder app={selectedApp} />
-        </div>
-      </div>
-    </Layout>
+    <Wrapper>
+      <Navbar
+        onAppChange={handleAppSwitch}
+        onLangChange={handleLangSwitch}
+        initial={initialLang}
+      />
+      <Container>
+        <AppPlaceholder app={selectedApp} />
+      </Container>
+    </Wrapper>
   );
 }
--- apps/host/src/components/Navbar/Navbar.tsx
+++ apps/host/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Container } from "../Container";
+import { AppSwitcher, Container, LangSwitcher } from "components";
 import { Nav, NavBarContent } from "./style";
+import { AppItem, LangItem } from "types";
 
-export default function Navbar() {
+interface NavbarProps {
+  /**
+   * Function to be called when an app item is selected.
+   * @param {AppItem} app - The selected app item.
+   * @returns {void}
+   */
+  onAppChange: (app: AppItem) => void;
+  onLangChange: (Lang: LangItem) => void;
+  initial: LangItem;
+}
+
+export function Navbar({ onAppChange, onLangChange, initial }: NavbarProps) {
   return (
     <Nav>
       <Container>
-        <NavBarContent></NavBarContent>
+        <NavBarContent>
+          {/* Render the AppSwitcher component with the handleAppSwitch function as a prop */}
+          <AppSwitcher onAppChange={onAppChange} />
+          {/* Render the LangSwitcher component with the handleLangSwitch function as a prop */}
+          <LangSwitcher onLangChange={onLangChange} initial={initial} />
+        </NavBarContent>
       </Container>
     </Nav>
   );
