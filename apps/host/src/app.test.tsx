d1327b025d8c35827cb16ef33866d1f732546557
Gustavo Benitez <gustavo.benitez.developer@gmail.com>
2023-12-15 05:27:53 -0300

    Add styled components fix test

--- apps/host/src/app.test.tsx
+++ apps/host/src/app.test.tsx
@@ -4,11 +4,18 @@ import "@testing-library/jest-dom";
 
 import React from "react";
 import App from "./app";
+import { GlobalStyles, defaultTheme } from "ui-lib";
+import { ThemeProvider } from "styled-components";
 
 // Test to verify that the page loads and displays the "Micro-Frontends" text
 test("loads and displays host", async () => {
   // Rendering the main App component
-  render(<App />);
+  render(
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  );
 
   // Waiting for the component to finish loading any suspended data
   await waitFor(() => {
--- apps/host/src/components/AppSwitcher/AppSwitcher.test.tsx
+++ apps/host/src/components/AppSwitcher/AppSwitcher.test.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { AppSwitcher } from "./AppSwitcher";
 import { AppItem } from "types";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, defaultTheme } from "ui-lib";
 
 // Mocking the onAppChange function
 const mockOnAppChange = jest.fn();
@@ -9,7 +11,13 @@ const mockOnAppChange = jest.fn();
 // Test to ensure that onAppChange is called with the Rick and Morty app when the button is clicked
 test("should call onAppChange with Rick and Morty app when button is clicked", () => {
   // Rendering the AppSwitcher component with the mocked onAppChange function
-  const { getByTestId } = render(<AppSwitcher onAppChange={mockOnAppChange} />);
+
+  const { getByTestId } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <AppSwitcher onAppChange={mockOnAppChange} />
+    </ThemeProvider>
+  );
 
   // Locating the button and simulating a click event
   fireEvent.click(getByTestId("app-switcher-rick-and-porty-test-id"));
